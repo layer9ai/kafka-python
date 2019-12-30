c159f38af21f637cb6694fab92b6a593425b7c86
Dana Powers <dana.powers@gmail.com>
2019-12-29 16:04:53 -0800

    Do not block on sender thread join after timeout in producer.close() (#1974)

--- kafka/producer/kafka.py
+++ kafka/producer/kafka.py
@@ -452,7 +452,7 @@ class KafkaProducer(object):
         global log
         log = NullLogger()
 
-        self.close(timeout=0)
+        self.close()
 
     def close(self, timeout=None):
         """Close this producer.
@@ -491,14 +491,10 @@ class KafkaProducer(object):
                     self._sender.join(timeout)
 
         if self._sender is not None and self._sender.is_alive():
-
             log.info("Proceeding to force close the producer since pending"
                      " requests could not be completed within timeout %s.",
                      timeout)
             self._sender.force_close()
-            # Only join the sender thread when not calling from callback.
-            if not invoked_from_callback:
-                self._sender.join()
 
         self._metrics.close()
         try:
